5ab79d664197bde1c2ad9b9f631f07ddecaeee0c
Alimohammed360 <130058501+Alimohammed360@users.noreply.github.com>
2023-09-28 18:24:26 +0500

    Add files via upload

---
+++ chapter 20-30/app.js
@@ -0,0 +1,47 @@
+var allLower = userInput.toLowerCase(); // Q1/A1
+
+var x = "KarAChi";                       // Q2/A2
+var x = x.toLowerCase()
+
+var y = "iSlaMBad";                      // Q3/A3
+var y = y.toUpperCase()
+
+var normal = "KarAChi";                  // Q4/A4
+var lower = normal.toLowerCase()
+
+var arr = ["KaRAchI", "IsLamAbAd", "LahOre", "PEsHAwar"];
+var lower2 = arr[2].toLowerCase()       // Q5/A5
+
+alert(y.toUpperCase(y))
+
+var cityName = "kaRacHi";
+var capitalize = cityName.slice(0, 1).toUpperCase() + cityName.slice(1).toLowerCase()
+console.log(capitalize);
+
+// Chapter 2
+
+var sameWords = "captain"
+console.log(sameWords.slice(1,3));
+
+var ele = "elephant"
+var slice = ele.slice(2,6)
+
+console.log(slice);
+
+var string = "Find the number of characters in the string represented by a variable and assign the number to a second variable."
+var len = string.length
+
+console.log(len);
+var assign = string.slice(0,109)
+
+console.log(assign);
+
+var text = "To be or not to be.";
+var indx = text.indexOf("be")
+var indx2 = text.lastIndexOfindexOf("be")
+
+console.log(indx); // vale of indx is 3
+console.log(indx2); // vale of indx is 16
+
+
+
